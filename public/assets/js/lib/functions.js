0cb3e8332569534af2c28cef9b12a172d4580f78
n.grossier1@gmail.com <n.grossier1@gmail.com>
2025-04-04 11:55:40 +0200

    04_04_2025 11h55

---
+++ public/assets/js/lib/functions.js
@@ -0,0 +1,34 @@
+// La route API [ex: http://localhost:8000]
+const apiUrl = "https://api.rawg.io/api";
+
+// fonction asynchrone
+// si Async mettre await dans le traitement
+// ex route => /blog
+// options => { Accept: 'application/json' }
+/**
+ *
+ * @param {string} route
+ * @param {object} options - peut inclure une propriété "params" pour les query params
+ * @returns  {Promise}
+ */
+export async function fetchData({ route, api = apiUrl, options = {} }) {
+  // Préparation de l'entête 'headers' avec les clés - valeurs nécessaire pour
+  // l'appel [ Authorization: 'Bearer qsgfjhF8768768678QSJFDHQjhjèçèçè_èvcV ]
+  const headers = { Accept: "application/json", ...options.headers };
+  console.log(headers);
+  // appel methode native fetch [ appels API ]
+  let queryString = "";
+  if (options.params) {
+    queryString = "?" + new URLSearchParams(options.params).toString();
+    delete options.params;
+  }
+  const result = await fetch(`${api}${route}${queryString}`, {
+    ...options,
+    headers,
+  });
+
+  if (result.ok) {
+    return result.json();
+  }
+  throw new Error("Erreur serveur", { cause: result });
+}
---
+++ public/assets/js/scroll-to-top/scroll-to-top.js
@@ -0,0 +1,26 @@
+const main = document.querySelector("main");
+const scrollToTopButton = document.querySelector("#scroll-to-top");
+const root = document.documentElement;
+function posX() {
+  const pos = (root.clientWidth - main.clientWidth) / 2;
+  scrollToTopButton.style.right = `${pos}px`;
+}
+
+function scrollToTop() {
+  const scrollToTopButton = document.querySelector("#scroll-to-top");
+  const heroHeader = document.querySelector("#hero-header");
+  window.addEventListener("scroll", () => {
+    const height = heroHeader ? heroHeader.offsetHeight : 10;
+    if (scrollY >= height) {
+      scrollToTopButton.style.display = "block";
+    } else if (scrollY === 0) {
+      scrollToTopButton.style.display = "none";
+    }
+    scrollToTopButton.addEventListener("click", () => window.scrollTo(0, 0));
+  });
+}
+window.addEventListener("DOMContentLoaded", () => {
+  scrollToTop();
+  window.addEventListener("resize", posX);
+});
+posX();
